00955479b46222a4feccc1bd80794683a7f54069
danvisintainer <dan@danvisintainer.com>
2015-05-13 00:52:06 -0400

    add create method to movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,15 @@ class MoviesController < ApplicationController
   end
 
   def create
+    redirect_to '/login' if !current_user
 
+    itunes_response = Net::HTTP.get_response('itunes.apple.com', "/lookup?id=#{URI::encode(params["id"])}")
+    result = JSON.parse(itunes_response.body)["results"].first
+
+    new_movie = Movie.create(itunes_id: result['trackId'], name: result['name'], image_link: result['artworkUrl100'], user_id: current_user.id)
+
+    respond_to do |f|
+      f.js { }
+    end
   end
 end

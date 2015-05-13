543e5f146ca88127403c1cc1f1e0d46cca1bfe64
danvisintainer <dan@danvisintainer.com>
2015-05-12 22:04:02 -0400

    implement ajax request for itunes API search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
   def search
-    binding.pry
+    itunes_response = Net::HTTP.get_response('itunes.apple.com', "/search?entity=movie&term=#{URI::encode(params["name"])}")
+    @results = JSON.parse(itunes_response.body)
+
+    respond_to do |f|
+      f.js { }
+    end
   end
 end

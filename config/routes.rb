9830d2fb38661bbd30f8640bc41698fe893262db
danvisintainer <dan@danvisintainer.com>
2015-05-13 02:18:11 -0400

    add user show route

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
-  get '/users/:id' => 'users#show', :as => :user
+  get '/users/show' => 'users#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

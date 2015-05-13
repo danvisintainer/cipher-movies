0986c4f06a19338fec886bc562a4e1c341face9d
danvisintainer <dan@danvisintainer.com>
2015-05-13 00:15:13 -0400

    add create / destroy methods to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    # Authenticate user's password
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to projects_path
+    #If creating the session fails, redirect to login and display a message
+    else
+      flash[:notice] = "Failed to log in. Please try again."
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
 end

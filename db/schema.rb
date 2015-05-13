c4b557d475e83b2f5cbbf2af229403e0b38e4080
danvisintainer <dan@danvisintainer.com>
2015-05-12 21:47:32 -0400

    make DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318152520) do
+ActiveRecord::Schema.define(version: 20150513012633) do
+
+  create_table "movies", force: true do |t|
+    t.integer  "itunes_id"
+    t.string   "name"
+    t.string   "image_link"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"

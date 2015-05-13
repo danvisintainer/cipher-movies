d638b78832dafc7b5133515d57d6ee33b57a27fb
danvisintainer <dan@danvisintainer.com>
2015-05-13 00:20:52 -0400

    add login / signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,15 @@ Rails.application.routes.draw do
   post 'movies/search' => 'movies#search'
   post 'movies/create' => 'movies#create'
 
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/users/:id' => 'users#show', :as => :user
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
